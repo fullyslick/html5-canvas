e24670697b345c79523ed5b03b5176bac23e7d36
fullyslick <default@mail.com>
2018-04-16 16:36:12 +0300

    Create multiple objects animation with class

--- js/app.js
+++ js/app.js
@@ -2,30 +2,49 @@ var mycanvas = document.querySelector('#mycanvas'); // gets the canvas HTML elem
 
 var ctx = mycanvas.getContext('2d'); // gets the context of the canvas 2d or  3d
 
-// Holds the posX position of the rectangle.
-// Gets update in the interval
-let posX = 0;
+// Create class constructor for every kvadrat that will be made
+function Kvadrat(inputX, inputY, inputSpeed, inputColor) {
+  this.posX = inputX;
+  this.posY = inputY;
+  this.speed = inputSpeed;
+  this.color = inputColor;
+}
 
-// Decalre the background image object
-const chessGridImage = new Image();
-
-setInterval(function() {
-
+// Create class prototype to hold the move method
+Kvadrat.prototype.moveRight = function() {
   // Update the posX position
-  posX += 2;
+  this.posX += this.speed;
 
   // When the green rectangle goes out of canvas reset its posX postion to 0
   // This creates loop effect
-  if ( posX == 300) {
-    posX = 0;
+  if (this.posX == 300) {
+    this.posX = 0;
   }
 
-  // Redraws the background
+  // Redraw the kvadrat with a new position
+  ctx.fillStyle = this.color;
+  ctx.fillRect(this.posX, this.posY, 30, 30);
+}
+
+// Create the purviKvadrat object from constructor class
+const purviKvadrat = new Kvadrat(10, 10, 5, '#ccff00');
+
+// Create the vtoriKvadrat object from constructor class
+const vtoriKvadrat = new Kvadrat(20, 80, 10, '#ff0000');
+
+// Declare the background image object
+const chessGridImage = new Image();
+
+// Create the moving effect
+setInterval(function() {
+
+  // draw the background image -> chessGridImage line 36, line 51
   ctx.drawImage(chessGridImage, 0, 0, mycanvas.width, mycanvas.height);
 
-  // Redraws the rectangle with a new position
-  ctx.fillStyle = "#ccff00";
-  ctx.fillRect( posX, 120, 30, 30);
+  // Call the prototype method to move purviKvadrat & vtoriKvadrat
+  purviKvadrat.moveRight();
+  vtoriKvadrat.moveRight();
+
 }, 30);
 
 // Set the source file of the  background image object
