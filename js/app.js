9ed613e982fa8e979e19fa3e385821ef737943db
fullyslick <default@mail.com>
2018-04-16 15:56:27 +0300

    Show example of simple animation

--- js/app.js
+++ js/app.js
@@ -2,8 +2,24 @@ var mycanvas = document.querySelector('#mycanvas'); // gets the canvas HTML elem
 
 var ctx = mycanvas.getContext('2d'); // gets the context of the canvas 2d or  3d
 
-ctx.fillStyle = "#000000";
-ctx.fillRect(0, 0, mycanvas.width, mycanvas.height );
 
-ctx.fillStyle = "#ccff00";
-ctx.fillRect(120, 120, 30, 30);
+(function justMoving() {
+
+  // Holds the x position of the rectangle.
+  // Gets update in the interval
+  let x = 0;
+
+  setInterval(function() {
+
+    // Update the x position
+    x += 2;
+
+    // Redraws the background
+    ctx.fillStyle = "rgba(0, 0, 0, 0.2)";
+    ctx.fillRect(0, 0, mycanvas.width, mycanvas.height);
+
+    // Redraws the rectangle with a new position
+    ctx.fillStyle = "#ccff00";
+    ctx.fillRect(x, 120, 30, 30);
+  }, 30);
+}());
